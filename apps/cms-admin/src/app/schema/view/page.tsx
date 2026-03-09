04b466456849087ae7ac9f68aab1791fa52e77a0
Jorge Soucasaux Monteiro <jorge.monteiro@vodafone.com>
2026-03-09 19:55:44 +0000

    schema view UI

--- apps/cms-admin/src/app/schema/view/page.tsx
+++ apps/cms-admin/src/app/schema/view/page.tsx
@@ -6,6 +6,28 @@ import React from 'react'
 import { Table } from '@contentful/f36-components'
 
 export default function SchemaViewPage() {
+  const getFieldType = (f: any) => {
+    let type = f.type
+    if (f.items) {
+      type += ` (${f.items.type}`
+      if (f.items.validations) {
+        const linkContentType = f.items.validations.find(
+          (v: any) => v.linkContentType
+        )?.linkContentType
+        if (linkContentType && linkContentType.length > 0) {
+          type += ` of ${linkContentType.join(', ')}`
+        }
+      }
+      type += ')'
+    } else if (f.validations) {
+      const linkContentType = f.validations.find((v: any) => v.linkContentType)?.linkContentType
+      if (linkContentType && linkContentType.length > 0) {
+        type += ` (${linkContentType.join(', ')})`
+      }
+    }
+    return type
+  }
+
   return (
     <div>
       <PageTitle title='Schema View' />
@@ -25,22 +47,15 @@ export default function SchemaViewPage() {
           {contentTypes.map((ct: any) => (
             <React.Fragment key={ct.id}>
               <Table.Row>
-                <Table.Cell>
-                  <div>
-                    <div>
-                      <strong>{ct.name}</strong>
-                    </div>
-                    <pre>{ct.id}</pre>
-                    <div>
-                      <em>{ct.description ?? ''}</em>
-                    </div>
+                <Table.Cell colSpan={6}>
+                  <div className='flex space-x-4'>
+                    <strong className='text-lg'>{ct.name}</strong>
+                    <strong className='text-lg text-danger'>
+                      <pre>{ct.id}</pre>
+                    </strong>
                   </div>
+                  <em>{ct.description ?? ''}</em>
                 </Table.Cell>
-                <Table.Cell></Table.Cell>
-                <Table.Cell></Table.Cell>
-                <Table.Cell></Table.Cell>
-                <Table.Cell></Table.Cell>
-                <Table.Cell></Table.Cell>
               </Table.Row>
 
               {Array.isArray(ct.fields) && ct.fields.length > 0 ? (
@@ -49,10 +64,7 @@ export default function SchemaViewPage() {
                     <Table.Cell></Table.Cell>
                     <Table.Cell>{f.name}</Table.Cell>
                     <Table.Cell>{f.id}</Table.Cell>
-                    <Table.Cell>
-                      {f.type}
-                      {f.items ? ` (${f.items.type})` : ''}
-                    </Table.Cell>
+                    <Table.Cell>{getFieldType(f)}</Table.Cell>
                     <Table.Cell>{f.required ? 'Yes' : 'No'}</Table.Cell>
                     <Table.Cell>{f.localized ? 'Yes' : 'No'}</Table.Cell>
                   </Table.Row>
