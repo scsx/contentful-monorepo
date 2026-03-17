cf71bfde44c89b9f45525f6876a6c8c9d4260c69
Jorge Soucasaux Monteiro <jorge.monteiro@vodafone.com>
2026-03-17 16:41:38 +0000

    Home

--- apps/contentful-app/components/locations/entry-sidebar/Sidebar.tsx
+++ apps/contentful-app/components/locations/entry-sidebar/Sidebar.tsx
@@ -30,9 +30,11 @@ const Sidebar: React.FC = () => {
 
         <Tabs.Panel id='second'>
           <Paragraph marginTop='spacingM' marginBottom='spacingM'>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
               src='https://www.estadao.com.br/resizer/v2/AZMGCEQCVFNPFBDI4PTSUV6GTU.jpg?quality=80&auth=80cfeced1b5f0fb585922c5fdfe8c9bf372e817d07aac11bf7a035995da55989&width=1200&height=1200&smart=true'
               style={{ width: '100%', height: 'auto' }}
+              alt='Homer'
             />
           </Paragraph>
         </Tabs.Panel>
--- apps/contentful-app/components/locations/home/HomeDisplay.tsx
+++ apps/contentful-app/components/locations/home/HomeDisplay.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Paragraph } from '@contentful/f36-components'
 
 const HomeDisplay = () => {
   return (
-    <div>HomeDisplay</div>
+    <div>
+      <Paragraph marginTop='spacingM' marginBottom='spacingM'>
+        This is Home Display
+      </Paragraph>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src='https://www.estadao.com.br/resizer/v2/AZMGCEQCVFNPFBDI4PTSUV6GTU.jpg?quality=80&auth=80cfeced1b5f0fb585922c5fdfe8c9bf372e817d07aac11bf7a035995da55989&width=1200&height=1200&smart=true'
+        style={{ width: '33%', height: 'auto' }}
+        alt='Homer'
+      />
+    </div>
   )
 }
 
