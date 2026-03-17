712db3360a068424637712415d83da2a2b587b2c
Jorge Soucasaux Monteiro <jorge.monteiro@vodafone.com>
2026-03-17 17:24:58 +0000

    Update index.tsx

--- apps/contentful-app/pages/index.tsx
+++ apps/contentful-app/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { locations } from '@contentful/app-sdk'
 import { useSDK } from '@contentful/react-apps-toolkit'
 import ConfigScreen from '@/components/locations/ConfigScreen'
@@ -14,13 +14,9 @@ const ComponentLocationSettings = {
 const App = () => {
   const sdk = useSDK()
 
-  const Component = useMemo(() => {
-    for (const [location, component] of Object.entries(ComponentLocationSettings)) {
-      if (sdk.location.is(location)) {
-        return component
-      }
-    }
-  }, [sdk.location])
+  const Component = Object.entries(ComponentLocationSettings).find(([location]) =>
+    sdk.location.is(location)
+  )?.[1]
 
   return Component ? <Component /> : <div>Unknown location</div>
 }
