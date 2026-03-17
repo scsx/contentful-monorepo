16c0add34fcb62d349292ff7766564d12e6891dc
Jorge Soucasaux Monteiro <jorge.monteiro@vodafone.com>
2026-03-17 11:47:30 +0000

    Update Sidebar.tsx

--- apps/contentful-app/components/locations/entry-sidebar/Sidebar.tsx
+++ apps/contentful-app/components/locations/entry-sidebar/Sidebar.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import * as React from 'react'
 import { useSDK } from '@contentful/react-apps-toolkit'
+import { Tabs, Paragraph } from '@contentful/f36-components'
 
 const Sidebar: React.FC = () => {
   const sdk = useSDK()
 
+  const contentType = sdk.contentType.sys.id
+  const entryId = sdk.entry.getSys().id
+
+  // Stupid TS fix.
+  const TabsComponent = Tabs as unknown as React.FC<any>
+  const TabsList = Tabs.List as unknown as React.FC<any>
+  const TabsTab = Tabs.Tab as unknown as React.FC<any>
+  const TabsPanel = Tabs.Panel as unknown as React.FC<any>
+
   return (
-    <div style={{ padding: '16px', backgroundColor: '#f3f4f5', borderRadius: '4px' }}>
-      <h3 style={{ margin: '0 0 12px 0' }}>My Sidebar App</h3>
-      <p style={{ margin: '8px 0' }}>This is showing in the entry sidebar!</p>
-      <p style={{ margin: '8px 0', fontSize: '12px', color: '#666' }}>Location: entry-sidebar</p>
+    <div style={{ padding: '5px 10px', backgroundColor: 'gold', borderRadius: '4px' }}>
+      <TabsComponent defaultTab='first'>
+        <TabsList>
+          <TabsTab panelId='first'>First</TabsTab>
+          <TabsTab panelId='second'>Second</TabsTab>
+        </TabsList>
+
+        <TabsPanel id='first'>
+          <Paragraph>I love Forma 36 design system.</Paragraph>
+        </TabsPanel>
+        <TabsPanel id='second'>Content for the second tab</TabsPanel>
+      </TabsComponent>
+
+      <div>
+        <p>CT: {contentType}</p>
+        <p>ID: {entryId}</p>
+      </div>
     </div>
   )
 }
